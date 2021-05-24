36cdbe80b173a03f13fdcdd61a1effb1b9efc8ba
Mac 17 <brayan.champi@tecsup.edu.pe>
2021-05-24 18:32:15 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Mac 17 on 5/24/21.
+//  Copyright © 2021 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
